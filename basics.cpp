33f3ae59e714a221a148acaf6a9ec0aea0105a38
nithesh <rnithesh46@gmail.com>
2026-02-02 16:41:27 +0530

    basics

---
+++ basics.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    // cout << "Hello" << "\n";
+    // int x,y;
+    // cin >>x>>y;
+    // cout << "value of x:"<< x << " value of y:"<< y << "\n";
+
+    //string doesnot take anything after sapce,so we have declared two string
+    // string s1,s2;
+    // cin >> s1>>s2;
+    // cout << s1 <<" "<<s2<<endl;
+
+    //to printline entire line
+    // string str;
+    // getline(cin,str);
+    // cout<<str<<endl;
+
+    // int age;
+    // cin>>age;
+    // if(age>=18){
+    //     cout<<"adult"<<endl;
+    // }
+    // if(age<18){
+    //     cout<<"not adult"<<endl;
+    // }
+
+    // int day;
+    // cin>>day;
+    // switch(day){
+    //     case 1:cout<<"monday";
+    //             break;
+    //     case 2:cout<<"tuesday";
+    //             break;
+    //     case 3:cout<<"wednesday";
+    //             break;
+    //     case 4:cout<<"thursday";
+    //             break;
+    //     case 5:cout<<"friday";
+    //             break;
+    //     case 6:cout<<"saturday";
+    //             break;
+    //     case 7:cout<<"sunday";
+    //             break;
+    //     default:cout<<"invalid";
+    // }
+
+    return 0;
+
+}
