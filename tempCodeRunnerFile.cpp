e50ff790943542603265e6667fb5aa276fd34087
nithesh <rnithesh46@gmail.com>
2026-02-03 20:52:49 +0530

    unknown

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+int space=2*(n-1);
+    // for(int i=0;i<n;i++){
+    //     //star
+    //     for(int j=0;j<=i;j++){
+    //         cout<<"*";
+    //     }
+    //     // space
+    //     for(int j=0;j<space;j++){
+    //         cout<<" ";
+    //     }
+    //     //star
+    //     for(int j=0;j<=i;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    //     space-=2;
+    // }
