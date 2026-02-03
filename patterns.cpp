72cf32933bf873864854bb0505e196ee84f88a6b
nithesh <rnithesh46@gmail.com>
2026-02-03 20:51:38 +0530

    patterns

--- patterns.cpp
+++ patterns.cpp
@@ -8,7 +8,6 @@ void print1(int n){
         cout<<endl;
     }
 }
-
 void print2(int n){
     for(int i=0;i<n;i++){
         for(int j=0;j<=i;j++){
@@ -17,7 +16,6 @@ void print2(int n){
         cout<<endl;
     }
 }
-
 void print3(int n){
     for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
@@ -26,7 +24,6 @@ void print3(int n){
         cout<<endl;
     }
 }
-
 void print4(int n){
     for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
@@ -35,7 +32,6 @@ void print4(int n){
         cout<<endl;
     }
 }
-
 void print5(int n){
     for(int i=0;i<n;i++){
         for(int j=0;j<n-i;j++){
@@ -44,7 +40,6 @@ void print5(int n){
         cout<<endl;
     }
 }
-
 void print6(int n){
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n-i+1;j++){
@@ -53,7 +48,6 @@ void print6(int n){
         cout<<endl;
     }
 }
-
 void print7(int n){
     for(int i=0;i<n;i++){
         //spaces
@@ -71,7 +65,6 @@ void print7(int n){
         cout<<endl;
     }
 }
-
 void print8(int n){
     for(int i=0;i<n;i++){
         //spaces
@@ -89,7 +82,6 @@ void print8(int n){
         cout<<endl;
     }
 }
-
 void print9(int n){
     //p7
     for(int i=0;i<n;i++){
@@ -124,18 +116,178 @@ void print9(int n){
         cout<<endl;
     }
 }
-int i;
 void print10(int n){
     for(int i=1;i<=2*n-1;i++){
         int stars=i;
+        if(i>n) stars=2*n-i;
         for(int j=1;j<=stars;j++){
             cout<< "* ";
-            if(i>n) stars=2*n-i;
         }
         cout<<endl;
     }
 }
-
+void print11(int n){
+    for(int i=0;i<n;i++){
+        int start;
+        if(i%2==0) start=1;
+        else start=0;
+        for(int j=0;j<=i;j++){
+            cout<< start;
+            start=1-start;
+        }
+        cout<<endl;
+    }
+}
+void print12(int n){
+    int spaces=2*(n-1);
+    for(int i=1;i<=n;i++){
+        //number
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        //space
+        for(int j=1;j<=spaces;j++){
+            cout<<" ";
+        }
+        //number
+        for(int j=i;j>=1;j--){
+            cout<<j;
+        }
+        cout<<endl;
+        spaces-=2;
+    }
+}
+void print13(int n){
+    int num=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<num<<" ";
+            num++;
+        }
+        cout<<endl;
+    }
+}
+void print14(int n){
+    for(int i=0;i<n;i++){
+        for(char ch='A';ch<='A'+i;ch++){
+            cout<<ch;
+        }
+        cout<<endl;
+    }
+}
+void print15(int n){
+    for(int i=1;i<=n;i++){
+        for(char ch='A';ch<='A'+(n-i);ch++){
+            cout<<ch;
+        }
+        cout<<endl;
+    }
+}
+void print16(int n){
+    for(int i=0;i<n;i++){
+        char ch='A'+i;
+        for(int j=0;j<=i;j++){
+            cout<<ch;
+        }
+        cout<<endl;
+    }
+}
+void print17(int n){
+    for(int i=0;i<n;i++){
+        //spaces
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        //character
+        char ch='A';
+        int breakpoint=(2*i+1)/2;
+        for(int j=0;j<2*i+1;j++){
+            cout<<ch;
+            if(j<breakpoint) ch++;
+            else ch--;
+        }
+        //spaces
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+void print18(int n){
+    for(int i=0;i<n;i++){
+        for(char ch='E'-i;ch<='E';ch++){
+            cout<<ch;
+        }
+        cout<<endl;
+    }
+}
+void print19(int n){
+    for(int i=0;i<n;i++){
+        //star
+        for(int j=0;j<n-i;j++){
+            cout<<"*";
+        }
+        //space
+        for(int j=0;j<2*i;j++){
+            cout<<" ";
+        }
+        //star
+        for(int j=0;j<n-i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    int space=2*(n-1);
+    for(int i=0;i<n;i++){
+        //star
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        // space
+        for(int j=0;j<space;j++){
+            cout<<" ";
+        }
+        //star
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+        space-=2;
+    }
+}
+void print20(int n){
+    int space=2*n-2;
+   for(int i=1;i<=2*n-1;i++){
+        int stars=i;
+        if(i>n) stars=2*n-i;
+        //stars
+        for(int j=1;j<=stars;j++){
+            cout<< "*";
+        }
+        //space
+        for(int j=1;j<=space;j++){
+            cout<< " ";
+        }
+        //stars
+        for(int j=1;j<=stars;j++){
+            cout<< "*";
+        }
+        cout<<endl;
+        if(i<n) space-=2;
+        else space+=2;
+    }
+}
+void print21(int n){
+   for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==0||j==0||i==n-1|j==n-1){
+                cout<< "*";
+            }
+            else cout<<" ";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
@@ -144,7 +296,7 @@ int main(){
     for(int i=0;i<t;i++){
         int n;
         cin>>n;
-        print10(n);
+        print21(n);
     }
 
     return 0;
