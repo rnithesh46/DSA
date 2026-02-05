683d9b3214b24c3bb71adf5454883816bdef3352
nithesh <rnithesh46@gmail.com>
2026-02-05 15:23:08 +0530

    math donee

--- math.cpp
+++ math.cpp
@@ -7,8 +7,8 @@ using namespace std;
 int main(){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
-    int n;
-    cin>>n;
+    int n1,n2;
+    cin>>n1>>n2;
 
     //return count of digits
     // int co=count(n);
@@ -55,19 +55,64 @@ int main(){
     //     if(n%i==0) cout<<i<<" ";
     // }
 
-    vector<int> v;
-    for(int i=1;i<=sqrt(n);i++){
-        if(n%i==0){
-            v.push_back(i);
-            if((n/i)!=i){
-                v.push_back(n/i);
-            }
-        }
-    }
-    sort(v.begin(),v.end());
-    for(auto it:v){
-        cout<<it<<" ";
-    }
+    // vector<int> v;
+    // for(int i=1;i<=sqrt(n);i++){
+    //     if(n%i==0){
+    //         v.push_back(i);
+    //         if((n/i)!=i){
+    //             v.push_back(n/i);
+    //         }
+    //     }
+    // }
+    // sort(v.begin(),v.end());
+    // for(auto it:v){
+    //     cout<<it<<" ";
+    // }
 
+    //prime number
+    // int count=0;
+    // for(int i=1;i<=n;i++){
+    //     if(n%i==0) count++; 
+    // }
+    // if(count==2) cout<<n<<" is a Prime number";
+    // else cout<<n<<" is not a Prime number";
+    
+    // int count=0;
+    // for(int i=1;i*i<=n;i++){
+    //     if(n%i==0){
+    //         count++;
+    //         if((n/i)!=i) {
+    //             count++;
+    //         }
+    //     } 
+        
+    // }
+    // if(count==2) cout<<n<<" is a Prime number";
+    // else cout<<n<<" is not a Prime number";
+
+    //gcd/hcf
+    // int gcd;
+    // for(int i=1;i<=min(n1,n2);i++){
+    //     if(n1%i==0 && n2%i==0) 
+    //     gcd=i;
+    // }
+    // cout<<gcd;
+
+    // for(int i=min(n1,n2);i>=1;i--){
+    //     if(n1%i==0 && n2%i==0) {
+    //         cout<<i<<" ";
+    //         break;
+    //     }
+    // }
+
+    //Eculidean algo
+    while(n1>0 && n2>0){
+        if(n1>n2) n1=n1%n2;
+        else n2=n2%n1;
+
+    }
+    if(n1==0) cout<<n2;
+    else cout<<n1;
+    
     return 0;
 }
