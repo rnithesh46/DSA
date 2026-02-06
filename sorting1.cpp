076ff57a1fed221dce6e6d8450e20c693e239fcb
nithesh <rnithesh46@gmail.com>
2026-02-06 16:52:18 +0530

    selection bubble

---
+++ sorting1.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+void selection_sort(int arr[],int n){
+    for(int i=0;i<=n-2;i++){
+        int min=i;
+        for(int j=i;j<=n-1;j++){
+            if(arr[j]<arr[min]) min=j;
+        }
+        int temp=arr[min];
+        arr[min]=arr[i];
+        arr[i]=temp;
+    }
+}
+void bubble_sort(int arr[],int n){
+    for(int i=n-1;i>=0;i--){
+        int didswap=0;
+        for(int j=0;j<=i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+                didswap=1;
+            }
+        }
+        if(didswap==0) break;
+        cout<<"runs"<<"\n";
+    }
+}
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    // selection_sort(arr,n);
+    bubble_sort(arr,n);
+    for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+}
