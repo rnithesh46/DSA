68c40ec41c050a705e10f162a8e677fbe257ae86
nithesh <rnithesh46@gmail.com>
2026-02-06 18:40:45 +0530

    sorting1 done

--- sorting1.cpp
+++ sorting1.cpp
@@ -26,6 +26,18 @@ void bubble_sort(int arr[],int n){
         cout<<"runs"<<"\n";
     }
 }
+void insertion_sort(int arr[],int n){
+    for(int i=0;i<=n-1;i++){
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+            int temp=arr[j-1];
+            arr[j-1]=arr[j];
+            arr[j]=temp;
+            j--;
+            cout<<"run"<<"\n";
+        }
+    }
+}
 int main(){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
@@ -34,6 +46,7 @@ int main(){
     int arr[n];
     for(int i=0;i<n;i++) cin>>arr[i];
     // selection_sort(arr,n);
-    bubble_sort(arr,n);
+    // bubble_sort(arr,n);
+    insertion_sort(arr,n);
     for(int i=0;i<n;i++) cout<<arr[i]<<" ";
 }
