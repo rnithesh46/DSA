87835aaf3c5914fe256a97699d3242f5f73074c6
nithesh <rnithesh46@gmail.com>
2026-02-09 15:47:43 +0530

    qs done

--- sorting2.cpp
+++ sorting2.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
+// Time Complexity: O(nlogn)
+// Space Complexity: O(n)
 void merge(vector<int> &arr,int low,int mid,int high){
     vector<int> temp;
     int left=low;
@@ -36,6 +38,34 @@ void ms(vector<int> &arr,int low,int high){
 void mergeSort(vector<int> &arr,int n){
     ms(arr,0,n-1);
 }
+
+int partition(vector<int> &arr,int low,int high){
+    int pivot=arr[low];
+    int i=low;
+    int j=high;
+    while(i<j){
+        while(arr[i]<=pivot && i<=high-1){
+            i++;
+        }
+        while(arr[j]>pivot && j>=low+1){
+            j--;
+        }
+        if(i<j) swap(arr[i],arr[j]);
+    }
+swap(arr[low],arr[j]);
+return j;
+}
+void qs(vector<int> &arr,int low,int high){
+    if(low<high){
+        int pIndex=partition(arr,low,high);
+        qs(arr,low,pIndex-1);
+        qs(arr,pIndex+1,high);
+    }
+}
+vector<int> quickSort(vector<int> arr){
+    qs(arr,0,arr.size()-1);
+    return arr;
+}
 int main(){
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
@@ -43,8 +73,7 @@ int main(){
     cin>>n;
     vector<int> arr(n);
     for(int i=0;i<n;i++) cin>>arr[i];
-    mergeSort(arr,n);
+    // mergeSort(arr,n);
+    arr=quickSort(arr);
     for(int element:arr) cout<<element<<" ";
 }
-// Time Complexity: O(nlogn)
-// Space Complexity: O(n)
