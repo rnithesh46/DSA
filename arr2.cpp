e173d5e2c65df904e2ff435ff21c9d0789dde867
nithesh <rnithesh46@gmail.com>
2026-02-14 16:42:04 +0530

    added

--- arr2.cpp
+++ arr2.cpp
@@ -48,17 +48,70 @@ void optimalRarrByD(int arr[],int n,int d){
 //     reversee(arr,0,n-1);
 //     for(int i=0;i<n;i++) cout<<arr[i]<<" ";
 // }
+
+vector<int> movezero(vector<int> &a,int n){
+    vector<int> temp;
+    for(int i=0;i<n;i++){
+        if(a[i]!=0){
+            temp.push_back(a[i]);
+        }
+    }
+    int nz=temp.size();
+    for(int i=0;i<nz;i++){
+        a[i]=temp[i];
+    }
+    for(int i=nz;i<n;i++){
+        a[i]=0;
+    }
+    return a;
+}
+vector<int> movezerooptimal(vector<int> &a,int n){
+    int j=-1;
+    for(int i=0;i<n;i++){
+        if(a[i]==0){
+            j=i;
+            break;
+        }
+    }
+    if (j==-1) return a;
+    for(int i=j+1;i<n;i++){
+        if(a[i]!=0){
+            swap(a[i],a[j]);
+            j++;
+        }
+    }
+    return a;
+}
+int linearsearch(vector<int> &a,int n,int num){
+    for(int i=0;i<n;i++){
+        if(a[i]==num) return i;
+    }
+    return -1;
+}
 int main(){
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     int n;
     cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++) cin>>arr[i];
-    // RarrBy1(arr,n);
-    int d;
-    cin>>d;
-    // RarrByD(arr,n,d);
-    optimalRarrByD(arr,n,d);
+    // int arr[n];
+    // for(int i=0;i<n;i++) cin>>arr[i];
+    // // RarrBy1(arr,n);
+    // int d;
+    // cin>>d;
+    // // RarrByD(arr,n,d);
+    // optimalRarrByD(arr,n,d);
+
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    // movezero(a,n);
+    // movezerooptimal(a,n);
+    // for(auto it:a){
+    //     cout<<it<<" ";
+    // }
+
+    int num;
+    cin>>num;
+    cout<<linearsearch(a,n,num);
+    
     return 0;
 }
